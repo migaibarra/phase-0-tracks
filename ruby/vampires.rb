fc8b7f6189280cc7b5bd6af0164be0c1277883b2
Miguel Angel Ibarra, Jr <miggs.ibarra@gmail.com>
2017-07-21 16:02:10 -0700

    Adding logic to store the responses for multiple surveys in variable arrays.

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -3,37 +3,63 @@
 
 puts "Hello welcome to Werewolf Inc. where we own the moonlight! We are in need of more werewolves."
 
-puts "What is your name?"
+puts "How many employees will be processed?"
+num_of_employees = gets.chomp.to_i # Setting the counter for the number of surveys that will be done.
 
-Name = gets.chomp # Prompting the individual to type their name and assigning it to a variable called Name
+# Initializing the survey results
 
-puts "What is your age?"
+Name = Array.new(num_of_employees)
+age = Array.new(num_of_employees)
+birthyear = Array.new(num_of_employees)
+garlic_bread_resp = Array.new(num_of_employees)
+insurance_resp = Array.new(num_of_employees)
+result = Array.new(num_of_employees)
 
-age = gets.chomp.to_i # Prompting the individual for their age and converting to an integer value before assigning to the age variable
+for num_of_employees in 1..num_of_employees # Creating a for loop to collect survey results
+  puts "What is your name?"
 
-puts "What year were you born?"
+  Name[num_of_employees-1] = gets.chomp # Prompting the individual to type their name and assigning it to a variable called Name
 
-birthyear = gets.chomp.to_i # Prompting for and assigning the birthyear variable
+  puts "What is your age?"
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you? (Y/N)"
+  age[num_of_employees-1] = gets.chomp.to_i # Prompting the individual for their age and converting to an integer value before assigning to the age variable
 
-garlic_bread_resp = gets.chomp # Prompting and assigning the response for garlic bread
+  puts "What year were you born?"
 
-puts "Would you like to enroll in the company's health insurance? (Y/N)"
+  birthyear[num_of_employees-1] = gets.chomp.to_i # Prompting for and assigning the birthyear variable
 
-insurance_resp = gets.chomp # Promping and assigning the response for health insurance
+  puts "Our company cafeteria serves garlic bread. Should we order some for you? (Y/N)"
 
-# The following logic tries to determine if the user is a vampire or not
-if Name == "Drake Cula" || Name == "Tu Fang"
-  result = "Definitely a vampire."
-elsif age == (2017 - birthyear) && garlic_bread_resp == "Y"
-  result = "Probably not a vampire."
-elsif age != (2017 - birthyear) && garlic_bread_resp == "N" && insurance_resp == "N"
-  result = "Almost certainly a vampire."
-elsif age != (2017 - birthyear) && garlic_bread_resp == "N"
-  result = "Probably a vampire."
-else
-  result = "Results inconclusive."
+  garlic_bread_resp[num_of_employees-1] = gets.chomp # Prompting and assigning the response for garlic bread
+
+  puts "Would you like to enroll in the company's health insurance? (Y/N)"
+
+  insurance_resp[num_of_employees-1] = gets.chomp # Promping and assigning the response for health insurance
+
+  # The following logic tries to determine if the user is a vampire or not
+  if Name[num_of_employees-1] == "Drake Cula" || Name[num_of_employees-1] == "Tu Fang"
+    result[num_of_employees-1] = "Definitely a vampire."
+  elsif age[num_of_employees-1] == (2017 - birthyear[num_of_employees-1]) && garlic_bread_resp[num_of_employees-1] == "Y"
+    result[num_of_employees-1] = "Probably not a vampire."
+  elsif age[num_of_employees-1] != (2017 - birthyear[num_of_employees-1]) && garlic_bread_resp[num_of_employees-1] == "N" && insurance_resp[num_of_employees-1] == "N"
+    result[num_of_employees-1] = "Almost certainly a vampire."
+  elsif age[num_of_employees-1] != (2017 - birthyear[num_of_employees-1]) && garlic_bread_resp[num_of_employees-1] == "N"
+    result[num_of_employees-1] = "Probably a vampire."
+  else
+    result[num_of_employees-1] = "Results inconclusive."
+  end
+
+  # Outputing the vampire determination
+  puts result[num_of_employees-1]
+
+  # Now subtracting one from the counter
+  num_of_employees -= 1
 end
 
-puts result
+# The following was used to debug the code
+# puts Name
+# puts age
+# puts birthyear
+# puts garlic_bread_resp
+# puts insurance_resp
+# puts result
