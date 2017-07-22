c1d2e8eeec623e9bd647282709da5a5b712bc7a3
Miguel Angel Ibarra, Jr <miggs.ibarra@gmail.com>
2017-07-22 11:48:31 -0700

    Added code to print to console.

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -4,7 +4,7 @@ hamster_name = gets.chomp
 
 puts "How loud is this hamster on a scale of 1 to 10?"
 
-volume_level = gets.chomp
+volume_level = gets.chomp.to_f
 
 puts "What color fur does this hamster have?"
 
@@ -16,15 +16,15 @@ good_candidate = gets.chomp
 
 puts "About how old is this hamster?"
 
-estimated_age = gets.chomp
+estimated_age = gets.chomp.to_f
 
-if estimated_age.empty?
-  estimated_age = nil
-else
-end
+# if estimated_age.empty?
+#   estimated_age = nil
+# else
+# end
 
-puts "#{hamster_name}"
-puts "#{volume_level}"
-puts "#{fur_color}"
-puts "#{good_candidate}"
-puts "#{estimated_age}"
+puts "Hamster Name:   #{hamster_name}"
+puts "Volume Level:   #{volume_level}"
+puts "Fur Color:      #{fur_color}"
+puts "Good Candidate: #{good_candidate}"
+puts "Estimated Age:  #{estimated_age}"
