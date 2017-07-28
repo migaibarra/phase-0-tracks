a62eba149f10cefe5829a1310aa9518a5866e292
Miguel Angel Ibarra, Jr <miggs.ibarra@gmail.com>
2017-07-27 17:54:15 -0700

    added some comments to the code to make the file more readable.

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,8 +1,15 @@
 # Release 0:
+# Search method to look for a number within an array of numbers and
+# outputting the index of that number
+
 def search_array(array, number)
-    search_result = Array.new
-    count = 0
+    search_result = Array.new # Defining a search result array
+    count = 0 # count to loop through array
+
+    # The following logic goes into the array using the counter
     while count < array.length
+    	# If the search number matches a value in the array return that index 
+    	# Pushing it to the search_result array
         if number == array[count]
             return search_result.push(count)
             count += 1
@@ -11,53 +18,61 @@ def search_array(array, number)
         end
     end
 end
-array = [23, 543, 24, 2, 9, 17]
+array = [23, 543, 24, 2, 9, 17] # An arbitrary test array
 p search_array(array, 24)
+# Should output [2] for search value of 24
+# Should output nil for search value of 3
 
 # Release 1:
+# Defining Fibinacci Method for a number 
 def fib_generator(number)
-    fib_array = Array.new(number)
-    counter = 0
+    fib_array = Array.new(number) # Create a new array of the size of the defined number
+    counter = 0 # Setting a counter equal to zero to set up a loop
+    # Logic to fill in the empty array with Fibinacci numbers
     while counter < fib_array.length
-        if counter == 0
-            fib_array[counter] = counter
+        if counter == 0 # Need to define the first number as zero
+            fib_array[counter] = counter # Note counter is zero for first loop
             counter += 1
-        elsif counter == 1
-            fib_array[counter] = counter
+        elsif counter == 1 # Need to define the second number as one
+            fib_array[counter] = counter # Note counter is one for second loop
             counter += 1
         else
-        fib_array[counter] = (fib_array[counter -1] + fib_array[counter -2])
-        counter += 1
+        	# Define all other numbers as the last sum of the last two elements in the array
+	        fib_array[counter] = (fib_array[counter -1] + fib_array[counter -2]) 
+	        counter += 1
         end
     end  
     return fib_array  
 end
-puts fib_generator(100)
+puts fib_generator(100) # Outputs 218922995834555169026 for an input number of 100
 
 # Release 2: Bubble Sort (acending order)
 
+# printing a test array of arbitrary numbers
 p test_array = [51, 14, 9, 2, 87, 62, 17, 102, 204, 73, 100, 281]
+
 def bubble_sort(array)
-    counter = 0
+    counter = 0 # Setting a counter to loop into some logic
     loop do
         swapped = false
         while counter < (array.length) -1
-        # loop do
+        	# Swapping values if the current value of an element is greater than the value of the next element in the array
             if array[counter] > array[counter+1]
 	            array[counter], array[counter+1] = array[counter+1], array[counter]
-	            swapped = true
+	            swapped = true # flag that is set if the code is tripped up
             end
             counter += 1
-            p array
+            p array # Print for debugging purposes
         end
         if swapped == false
-            break
+            break # If no swapping occured at all, break
         end
-        bubble_sort(array)
+        bubble_sort(array) #reinsert the method within the method
 
     end
     return array
 end
+
 p bubble_sort(test_array)
 
 
