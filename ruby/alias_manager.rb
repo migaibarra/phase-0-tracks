894f4ddd33e542ef8121633de8ce6cce84264a77
Miguel Angel Ibarra, Jr <miggs.ibarra@gmail.com>
2017-07-29 15:35:27 -0700

    Finishing the impementation of the alias manager algorithm for Ex 5.5 with a prompt and a special scrambler.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -58,10 +58,10 @@ end
 
 #R1 Defining a method to Prompt the user to scramble their name
 def scrambled_prompt(input, names, aliases) #R1 and R2 (including names and aliases)
-	
+	flag = false #R2 Setting a flag for the while loop which helps store variables
 	#R1: Running a loop as a user interface
 	names.push(input) #R2 Pushing the name into the name array
-	loop do #R1
+	while flag == false #R2 This time running a while loop
 	#R1
 		scrambled_name = next_vowel(input.downcase) # Note making sure all letters are lowercase with .downcase method
 		puts "Your new Alias is:" #R1	
@@ -75,23 +75,26 @@ def scrambled_prompt(input, names, aliases) #R1 and R2 (including names and alia
 			p names #R2
 			puts "Scrambled Aliases:" #R2
 			p aliases #R2
-			spies = {names => aliases} #R2
-			return spies #R2
-			# break #R1 Using Break condition to get out of loop
 		else #R1
 			scrambled_prompt(scrambled_name, names, aliases) #R1 Reinsert the name into the prompt
 		end #R1
-		break #R1 Using Break conditions to get out of loop
+		flag = true #R2 Setting the flag to true to stop the while loop
+		# p spies
+		# return spies #R2
 	end #R1
+	# p spies
+	return {names => aliases} #R2 Returning the data as a hash
 end
 
 #R2 Setting up variables for the names and the aliases
 names = [] #R2 Empty names array
 aliases = [] #R2 Empty aliases array
 
-spies = scrambled_prompt(swapped_name, names, aliases)
 
-spies.each{|key,value| puts "#{key} is actually #{value}"}
+spies = scrambled_prompt(swapped_name, names, aliases) #R2 running the program in such a way to assign the returned hash to a variable called spies
+
+#R2 Running the each iterative method to attempt to show the data, see the note for Release 2 below:
+spies.each{|key,value| puts " The Name Array: \n #{key} \n Corresponds to the Alias Array: \n #{value} \n on an index to index basis"} #R2
 
 ## Release 1: User Interface Part
 #  Changes to the above code are denoted by '#R1' to show how the above code
@@ -100,3 +103,13 @@ spies.each{|key,value| puts "#{key} is actually #{value}"}
 ## Release 2: Storing the Aliases
 #  CHanges to the above code are denoded by '#R2' to show how the above code
 #  was changed to store the aliases
+
+## NOTE FOR RELEASE 2: The final hash variable named spies is not set up elegantly,
+#  The hash has only one key and only one value, however the key is composed of
+#  an array of input name strings and the value is an array of output scrambled names.
+#  This means that while the spies has has the each method run on the hash variable
+#  the each method acts on only ONE key and ONE value that contains an array of
+#  arbitrary size. More coding, or a different approach to the scrambled prompt
+#  would be necessary to split the array of strings into various keys; however,
+#  the implementation does demonstrate that the data is passed through the program
+#  and output can be put into a new variable.  
