0d67f5309f869d7509b770e0d26c6cc4a23f4fab
Miguel Angel Ibarra, Jr <miggs.ibarra@gmail.com>
2017-08-02 18:58:49 -0700

    Added setter methods in my Santa.rb file

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -10,18 +10,18 @@ class Santa
 		@gender = gender
 		@ethnicity = ethnicity
 		if gender == "male" || gender == "bigender" # Code to add a beard attribute if the santa is male or bigender
-			beard = true
+			@beard = true
 		else
-			beard = false
+			@beard = false
 		end
 		puts name
-		puts "Does this santa have a beard? #{beard}"
+		puts "Does this santa have a beard? #{@beard}"
+
+		# Variables not passed in on initialization
+		@reindeer_ranking = ["Rudolph","Dasher","Dancer","Prancer","Vixen",
+		 					"Comet", "Cupid","Donner", "Blitzen"]
+		@age = 0
 	end
-
-	reindeer_ranking = ["Rudolph","Dasher","Dancer","Prancer","Vixen",
-						"Comet", "Cupid","Donner", "Blitzen"]
-
-	age = 0
 	
 	# Defining a speak method that will have a Santa say a message
 	def speak
@@ -32,7 +32,49 @@ class Santa
 	def eat_milk_and_cookie(cookie_type)
 		puts "That was a good #{cookie_type}!"
 	end
+
+	# Adding Getter methods for my Santa Class:
+	def age # Returning Age outside the class
+		@age
+	end
+
+	def ethnicity # Returning ethnicity outside the class
+		@ethnicity
+	end
+
+	def reindeer_ranking
+		@reindeer_ranking
+	end
+
+	# Adding Setter methods to my Santa Class:
+	def celebrate_birthday=(dummy_variable)
+		@age += 1
+	end
+
+	def get_mad_at=(reindeer)
+		@reindeer_ranking.push(@reindeer_ranking.select{|reindeer_names|
+			reindeer_names == reindeer})
+	end
+
+
+	def gender=(new_gender) # Setter method to change gender
+		@gender = new_gender
+	end
+
 end
+
+# ## Release 2 Practice Code:
+
+# class Reindeer
+#   def initialize(name)
+#     @name = name
+#   end
+# end
+
+# reindeer = Reindeer.new("Blitzen")
+# reindeer.name
+
+# ## End of Release 2 Practice Code
 
 # Driver Code
 
@@ -59,3 +101,14 @@ example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer n
 example_genders.length.times do |i|
   santas << Santa.new(example_names[i], example_genders[i], example_ethnicities[i])
 end
+
+santas[3].celebrate_birthday = "dummy variable"
+puts santas[3].age
+
+# dillians_new_gender = "cisfemale"
+# dillians_gender = santas[0].gender(dillians_new_gender)
+# puts dillians_gender
+
+santas[0].get_mad_at = "Rudolph"
+puts santas[0].reindeer_ranking
+
