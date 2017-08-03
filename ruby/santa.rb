b22e1a3887d042e2f80db560fd435756c6f206fd
Miguel Angel Ibarra, Jr <miggs.ibarra@gmail.com>
2017-08-02 22:22:09 -0700

    Adding the final code for release 4 of Ex. 6.3

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -10,9 +10,9 @@ class Santa
 	attr_accessor :age, :reindeer_ranking, :gender
 
 	# Initializing with a message anytime an instance of a Santa class is created
-	def initialize(name, gender, ethnicity)
+	def initialize(gender, ethnicity)
 		puts "Initializing Santa instance..."
-		@name = name # Adding a name varialbe to our Santa
+		# @name = name # Adding a name varialbe to our Santa # Commenting out name to simplify the situation
 		@gender = gender
 		@ethnicity = ethnicity
 		if gender == "male" || gender == "bigender" # Code to add a beard attribute if the santa is male or bigender
@@ -20,13 +20,13 @@ class Santa
 		else
 			@beard = false
 		end
-		puts name
+		# puts name # Also getting rid of this reference to name
 		puts "Does this santa have a beard? #{@beard}"
 
 		# Variables not passed in on initialization
 		@reindeer_ranking = ["Rudolph","Dasher","Dancer","Prancer","Vixen",
 		 					"Comet", "Cupid","Donner", "Blitzen"]
-		@age = 0
+		@age = rand(0...140)
 	end
 	
 	# Defining a speak method that will have a Santa say a message
@@ -105,20 +105,31 @@ end
 # }
 
 santas = []
-example_names = ["Dillian", "Sarah", "Kris", "Michael", "Michaela", "Sasha", "Candy"]
-example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
-example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-example_genders.length.times do |i|
-  santas << Santa.new(example_names[i], example_genders[i], example_ethnicities[i])
+# example_names = ["Dillian", "Sarah", "Kris", "Michael", "Michaela", "Sasha", "Candy"]
+possible_genders = ["agender", "androgyne", "pangender", "hijra", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+possible_ethnicities = ["black", "White South African", "Nigerian", "Ethiopian", "Chinese", "Koean", "Mexican", "Zulu", "Vietnamese", "Thai", "Uyghur Chinese", "Italian", "Latino", "Indo-Aryan", "Punjabi", "Hindi", "Bengali", "Dravidian", "First Nation", "white", "Japanese-African", "Tibetan", "Mongolol", "Hui Chinese", "Norwegian", "Wopkaimin", "Papuan", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+
+puts "How many santas do you want to create?"
+num_of_santas = gets.chomp.to_i
+
+num_of_santas.times do
+  santas << Santa.new(possible_genders.sample, possible_ethnicities.sample)
 end
 
-puts santas[3].ethnicity
 
-santas[3].celebrate_birthday = "dummy variable"
-puts santas[3].age
+## Driver code to test some of the above variables
+#  Note this only works if you input at least 98 santas
 
-santas[0].get_mad_at = "Vixen"
-puts santas[0].reindeer_ranking
+# puts santas[3].ethnicity
+
+# santas[3].celebrate_birthday = "dummy variable"
+# puts santas[3].age
+
+# puts santas[5].age
+# puts santas[98].age
+
+# santas[0].get_mad_at = "Vixen"
+# puts santas[0].reindeer_ranking
 
 santas[0].new_gender = "cisfemale"
 puts santas[0].gender
