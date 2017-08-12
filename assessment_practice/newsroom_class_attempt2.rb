bbc29ad635d69d11982260288c0804c181a129b7
Miguel Angel Ibarra, Jr <miggs.ibarra@gmail.com>
2017-08-11 17:43:15 -0700

    added code for reporting salaries for Release 3 and 4

--- assessment_practice/newsroom_class_attempt2.rb
+++ assessment_practice/newsroom_class_attempt2.rb
@@ -5,12 +5,34 @@
 
 class Newsroom
 
-	attr_reader :name
+	attr_reader :name, :reporters, :overhead
 	attr_accessor :budget
 
 	def initialize(name, budget)
 		@name = name
 		@budget = budget
+		@reporters = Hash.new
+		@overhead = 0
+	end
+
+	# Release 2: Adding Reporters
+	def add_reporters(reporter, skills)
+		reporter_hash = {"Skills": skills}
+		@reporters.store(reporter, reporter_hash)
+	end
+
+	# Release 3: Determining salaries
+	def salary_for(reporter)
+		return reporter.length * 10000
+	end
+
+	# Release 4: Finding out total salaries
+	def total_salaries
+		# What to determine the total salaries as we add any new reporters
+		# First get the keys for the reporters
+		reporters = @reporters.keys
+		salaries = reporters.map {|reporter| salary_for(reporter)}
+		salaries.each {|salary| @overhead+=salary}
 	end
 end
 
@@ -18,9 +40,24 @@ end
 
 the_news = Newsroom.new("CNN", 300000)
 
-p the_news.name
+# p the_news.name
 
-p the_news.budget
+# p the_news.budget
 
 
+# # Release 2: Adding reporters
+the_news.add_reporters("Wolf Blitzer", ["investigating", "interviewing", "drinking coffee"])
+the_news.add_reporters("Anderson Cooper", ["reporting", "investigating", "not aging"])
+the_news.add_reporters("Rebecca Berg", ["talking politics", "polling"])
 
+# the_news.reporters.each{ |key, value| puts "#{key} can do the following: #{value.values.flatten.join(", ")}"}
+
+# Release 3: Determining the reporters' salaries
+
+# p the_news.salary_for("Anderson Cooper")
+
+# Release 4: Determining a newsroom total overhead
+
+p the_news.overhead
+the_news.total_salaries
+p the_news.overhead
