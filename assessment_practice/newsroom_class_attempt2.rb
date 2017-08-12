b800bf06ae5df25285fb91d5ba32da311c552479
Miguel Angel Ibarra, Jr <miggs.ibarra@gmail.com>
2017-08-11 20:14:02 -0700

    Adding the final required release (release 8) for my newsroom_class_attempt2.rb

--- assessment_practice/newsroom_class_attempt2.rb
+++ assessment_practice/newsroom_class_attempt2.rb
@@ -1,102 +1,113 @@
-## This exercise is for practice for the Phase 0 assessment
-#  Coded by Miguel A. Ibarra, Jr.
+	## This exercise is for practice for the Phase 0 assessment
+	#  Coded by Miguel A. Ibarra, Jr.
 
-# Release 1: Creating a Newsroom Class
+	# Release 1: Creating a Newsroom Class
 
-class Newsroom
+	class Newsroom
 
-	attr_reader :name, :reporters, :overhead
-	attr_accessor :budget
+		attr_reader :name, :reporters, :overhead, :matches
+		attr_accessor :budget
 
-	def initialize(name, budget)
-		@name = name
-		@budget = budget
-		@reporters = Hash.new
-		@overhead = 0
-	end
-
-	# Release 2: Adding Reporters
-	def add_reporters(reporter, skills)
-		reporter_hash = {"Skills": skills}
-		self.total_salaries
-		# @reporters.store(reporter, reporter_hash)
-		# @reporters.total_salaries
-		if self.has_budget?(reporter)
-			@reporters.store(reporter, reporter_hash)
-		else
-			return "Sorry, there is not enough budget to hire #{reporter}"
+		def initialize(name, budget)
+			@name = name
+			@budget = budget
+			@reporters = Hash.new
+			@overhead = 0
+			@matches = Array.new
 		end
-		# def has_budget?
-		# 	if @overhead + salary_for(reporter) > @budget
-		# 		puts "Sorry, there is not enough in the budget to have #{reporter} on the payroll"
-		# 	else
-		# 		@reporters.store(reporter, reporter_hash)
-		# 		self.total_salaries
-		# 	end
-		# end
-		# has_budget?
-	end
 
-	# Release 3: Determining salaries
-	def salary_for(reporter)
-		return reporter.length * 10000
-	end
+		# Release 2: Adding Reporters
+		def add_reporters(reporter, skills)
+			# reporter_hash = {"Skills": skills}
+			self.total_salaries
+			# @reporters.store(reporter, reporter_hash)
+			# @reporters.total_salaries
+			if self.has_budget?(reporter)
+				@reporters.store(reporter, skills)
+			else
+				return "Sorry, there is not enough budget to hire #{reporter}"
+			end
+			# def has_budget?
+			# 	if @overhead + salary_for(reporter) > @budget
+			# 		puts "Sorry, there is not enough in the budget to have #{reporter} on the payroll"
+			# 	else
+			# 		@reporters.store(reporter, reporter_hash)
+			# 		self.total_salaries
+			# 	end
+			# end
+			# has_budget?
+		end
 
-	# Release 4: Finding out total salaries
-	def total_salaries
-		# What to determine the total salaries as we add any new reporters
-		# First get the keys for the reporters
-		salaries = @reporters.keys.map {|reporter| salary_for(reporter)}
-		salaries.each {|salary| @overhead+=salary}
-	end
+		# Release 3: Determining salaries
+		def salary_for(reporter)
+			return reporter.length * 10000
+		end
 
-	# Release 5: Finding out if we have enough budget for reporters
-	def has_budget?(reporter)
-		if @overhead + salary_for(reporter) <= @budget 
-			return true
-		else
-			return false
+		# Release 4: Finding out total salaries
+		def total_salaries
+			# What to determine the total salaries as we add any new reporters
+			# First get the keys for the reporters
+			salaries = @reporters.keys.map {|reporter| salary_for(reporter)}
+			salaries.each {|salary| @overhead+=salary}
+		end
+
+		# Release 5: Finding out if we have enough budget for reporters
+		def has_budget?(reporter)
+			if @overhead + salary_for(reporter) <= @budget 
+				return true
+			else
+				return false
+			end
+		end
+
+		# Release 8: linking stories with the reporter
+		def find_reporters_with_skill(skill)
+			@reporters.each{|key,value|
+				if value.include?(skill);
+					@matches.push(key)
+				end}
 		end
 	end
 
-end
+	## DRIVER CODE ##
 
-## DRIVER CODE ##
+	the_news = Newsroom.new("CNN", 300000)
 
-the_news = Newsroom.new("CNN", 300000)
+	# p the_news.name
 
-# p the_news.name
-
-# p the_news.budget
+	# p the_news.budget
 
 
-# # Release 2: Adding reporters
-# the_news.add_reporters("Wolf Blitzer", ["investigating", "interviewing", "drinking coffee"])
-the_news.add_reporters("Anderson Cooper", ["reporting", "investigating", "not aging"])
-the_news.add_reporters("Rebecca Berg", ["talking politics", "polling"])
+	# # Release 2: Adding reporters
+	# the_news.add_reporters("Wolf Blitzer", ["investigating", "interviewing", "drinking coffee"])
+	the_news.add_reporters("Anderson Cooper", ["reporting", "investigating", "not aging"])
+	the_news.add_reporters("Rebecca Berg", ["reporting", "talking politics", "polling"])
 
-p the_news.reporters
+	p the_news.reporters
 
-# the_news.reporters.each{ |key, value| puts "#{key} can do the following: #{value.values.flatten.join(", ")}"}
+	# the_news.reporters.each{ |key, value| puts "#{key} can do the following: #{value.values.flatten.join(", ")}"}
 
-# Release 3: Determining the reporters' salaries
+	# Release 3: Determining the reporters' salaries
 
-# p the_news.salary_for("Anderson Cooper")
+	# p the_news.salary_for("Anderson Cooper")
 
-# # Release 4: Determining a newsroom total overhead
+	# # Release 4: Determining a newsroom total overhead
 
-# p the_news.overhead
-# the_news.total_salaries
-# p the_news.overhead
+	# p the_news.overhead
+	# the_news.total_salaries
+	# p the_news.overhead
 
-# Release 5: Testing code to avoid us going overbudget
+	# Release 5: Testing code to avoid us going overbudget
 
-p the_news.add_reporters("Marvin Zindler", ["finding slime in the ice-machine"])
+	p the_news.add_reporters("Marvin Zindler", ["finding slime in the ice-machine"])
 
-# Release 7: 
-puts "Welcome to the #{the_news.name} Newsroom!\n \n"
-puts "Your reporting team is:"
-the_news.reporters.each{ |key, value| 
-	puts "- #{key}, specializing in #{value.values.flatten.join(", ")}."
-}
-puts "Thank you for watching! Good night!"
+	# Release 7: 
+	puts "Welcome to the #{the_news.name} Newsroom!\n \n"
+	puts "Your reporting team is:"
+	the_news.reporters.each{ |key, value| 
+		puts "- #{key}, specializing in #{value.flatten.join(", ")}."
+	}
+	puts "Thank you for watching! Good night!"
+
+	the_news.find_reporters_with_skill("not aging")
+	p the_news.matches
