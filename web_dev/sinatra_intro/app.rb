8040b68936a0180f81e3ec296f8c2663241d11d7
Miguel Angel Ibarra, Jr <miggs.ibarra@gmail.com>
2017-08-27 22:37:57 -0700

    Added some notes for release1.txt regarding some questions about sinatra.

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -19,19 +19,19 @@ db.execute(create_contact_table)
 # Copied student ID and name from students table using the following SQL commands
 # in the SQL command line:
 
-# INSERT INTO contact (id, name)
-# SELECT id, name FROM students;
+# INSERT INTO contact (id, name) SELECT id, name FROM students;
 
 # The next few lines of code are run only once to fill in the database with fake
-# addresses:
+# addresses (Please note that this did not quite work the way I intended and 
+# everyone has the same addresss, so let's pretend they all live in a commune):
 
-def create_contact(db, address)
-  db.execute("INSERT INTO contact (address) FROM (?)", [address])
-end
+# def create_contact(db, address)
+#   db.execute("UPDATE contact SET address = (?)", [address])
+# end
 
-50.times do
-  create_contact(db, Faker::Address.street_address)
-end
+# 50.times do
+#   create_contact(db, Faker::Address.street_address)
+# end 
 
 # write a basic GET route
 # add a query parameter
@@ -72,3 +72,24 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+# writing a GET route that retrieves
+# The students addresses
+get '/contact' do
+	contact = db.execute("SELECT * FROM contact")
+	response = ""
+	contact.each do |contact|
+		response << "ID: #{contact['id']}<br>"
+		response << "Name: #{contact['name']}<br>"
+		response << "Address: #{contact['address']}<br><br>"
+	end
+	response
+end
+
+# writing a great job route to tell a person that they did a great job
+get '/great_job/:person' do
+	person = params[:person]
+	"Good job, #{person}!"
+end
+
+# writing a route to add two numbers together
